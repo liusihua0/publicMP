d9b7c538f489a021545b29c0525d3c26b64d982d
liusihua <445044902@qq.com>
2018-07-23 21:05:04 +0800

    3033

---
+++ src/page/adminList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="fillcontain">
+        adminList
+    </div>
+</template>
+
+<script>
+    export default {
+    	
+    }
+
+
+    
+</script>
+
+<style lang="less">
+	@import '../style/mixin';
+</style>
